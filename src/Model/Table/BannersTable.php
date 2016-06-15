9b5ffd7be3aa01e402c9da68927d4b402bc34afe
đan linh <an linh>
2016-06-15 14:58:49 +0700

    create model answer banner and footer

---
+++ src/Model/Table/BannersTable.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Banner;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Banners Model
+ *
+ */
+class BannersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('banners');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->requirePresence('image', 'create')
+            ->notEmpty('image');
+
+        return $validator;
+    }
+}
---
+++ src/Model/Table/FootersTable.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Footer;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Footers Model
+ *
+ */
+class FootersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('footers');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->requirePresence('description', 'create')
+            ->notEmpty('description');
+
+        $validator
+            ->requirePresence('phone', 'create')
+            ->notEmpty('phone');
+
+        $validator
+            ->requirePresence('fax', 'create')
+            ->notEmpty('fax');
+
+        return $validator;
+    }
+}
