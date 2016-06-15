380a0ade104d977bc8d5cb18719874d12a2c8985
đan linh <an linh>
2016-06-15 14:53:38 +0700

    create table footers answers banners and add column imageTonews

---
+++ config/Migrations/20160615022634_created_footers_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreatedFootersTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('footers');
+                                $table->addColumn('name', 'string', [
+                                    'default' => null,
+                                    'limit' => 255,
+                                    'null' => false,
+                                ]);
+                                $table->addColumn('description', 'text', [
+                                    'default' => null,
+                                    'null' => false,
+                                ]);
+                                 $table->addColumn('phone', 'string', [
+                                    'default' => null,
+                                    'null' => false,
+                                ]);
+                                  $table->addColumn('fax', 'string', [
+                                    'default' => null,
+                                    'null' => false,
+                                ]);
+                                $table->addColumn('created', 'datetime', [
+                                    'default' => null,
+                                    'null' => false,
+                                ]);
+                                $table->addColumn('modified', 'datetime', [
+                                    'default' => null,
+                                    'null' => false,
+                                ]);
+                                $table->create();
+    }
+}
